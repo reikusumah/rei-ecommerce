72dfce2862f9569bf9e46cf05777bb59755118f7
reikusumah <kusumah.rei@gmail.com>
2022-11-06 21:37:27 +0700

    Add Categories Section, and Category Item component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Annoucement from "../components/Annoucement";
+import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
 
@@ -9,6 +10,7 @@ const Home = () => {
       <Annoucement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   );
 };
