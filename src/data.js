e9fde5dd8861f48d1e909def98207d727f191c68
reikusumah <kusumah.rei@gmail.com>
2022-11-07 15:56:06 +0700

    create product list UI, styling Products and Product component

--- src/data.js
+++ src/data.js
@@ -43,3 +43,38 @@ export const categories = [
     title: "LIGHT JACKETS",
   },
 ];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.hN6So1Nf2RpIMMVqzBbe_gHaI4%26pid%3DApi&f=1&ipt=43bb84b283854fd3691bf51673a7ea4b36cf3c9f8e9447f7f1541a88ca048c36&ipo=images",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fdress%2Fdress_PNG53.png&f=1&nofb=1&ipt=cb694e5e7234ce349fdbb6eb447f180d6b26bacc82f732a93b5a157e963b58f4&ipo=images",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.obi3X73hahF9hBFIWf53nAHaJM%26pid%3DApi&f=1&ipt=a766f2bd1c702bc98e18eacbd1ca47a81689879f3dc9acf680fe82f2b0210c74&ipo=images",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
